f1d171c8b2d1477d7666e76d341d6c85c222d6bf
RyamSousa <ryam.sousa123@gmail.com>
2022-09-27 20:23:16 -0300

    :sparkles: added some unit tests for 'unique-id.service'

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Module 1
+- **describe**: The metlhod that contains the artifact that we will to test and all of
+    your tests(**it**)
 - For default the angular uses the jasmine as test framemework
 - The karma is respponible for execute the tests, he'is framework agnostic, can be used
       with anyother test framework (jasmine, Mocha, QUnit) 
+
+# Module 2
+- The execution of karma tests is random to avoid errors from shared instances between tests
+- Use the **BeforeEach** method to share objects instances between the tests executions
+- **withContext**: Add a context to the test for the jasmine show more informations about failed test
--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,12 +1,50 @@
 import { UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
+	let service: UniqueIdService = null;
+
+	beforeEach(() => {
+		service = new UniqueIdService();
+	});
+
 	it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
         should generete id when called with prefix`, () => {
 		const prefix = "app";
-		const service = new UniqueIdService();
 		const id = service.generateUniqueIdWithPrefix(prefix);
 
-		expect(id).toContain(`${prefix}-`);
+		expect(id.startsWith(`${prefix}-`)).toBeTrue();
+	});
+
+	it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+        should not generate duplicate IDs when called multiple times`, () => {
+		const prefix = "app";
+		const ids = new Set();
+
+		for (let i = 0; i < 10; i++) {
+			ids.add(service.generateUniqueIdWithPrefix(prefix));
+		}
+
+		expect(ids.size).toBe(10);
+	});
+
+	it(`${UniqueIdService.prototype.getNumberOfGeneretedUniqueIds.name}
+        should return the number of generatedIds when called`, () => {
+		const prefix = "app";
+
+		service.generateUniqueIdWithPrefix(prefix);
+		service.generateUniqueIdWithPrefix(prefix);
+
+		expect(service.getNumberOfGeneretedUniqueIds()).toBe(2);
+	});
+
+	it(`${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+        should throw when called with empty`, () => {
+		const emptyValues = [null, undefined, "", "0", "1"];
+
+		emptyValues.forEach((emptyValue) => {
+			expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+				.withContext(`Empty value: ${emptyValue}`)
+				.toThrow();
+		});
 	});
 });
