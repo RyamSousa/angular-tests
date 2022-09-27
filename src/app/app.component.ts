0205a643fcfcb786c9f2624c42893803a8202d4b
RyamSousa <ryam.sousa123@gmail.com>
2022-09-27 20:40:20 -0300

    :sparkles: added 'like-widget' component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'Angular testing';
+	title = "Angular testing";
+	public likes = 0;
+
+	public like(): void {
+		this.likes++;
+	}
 }
